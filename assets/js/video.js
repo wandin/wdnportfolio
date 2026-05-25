3abf679fb57eab27a97daae3e197c25fb485b342
Wanderson Lopes <78943275+wandin@users.noreply.github.com>
2026-05-25 17:46:15 -0300

    Delete assets directory

--- assets/js/video.js
+++
@@ -1,9 +0,0 @@
-function stopAllVideos(container) {
-  const iframes = container.querySelectorAll("iframe");
-
-  iframes.forEach(iframe => {
-    const src = iframe.src;
-    iframe.src = "";
-    iframe.src = src;
-  });
-}
